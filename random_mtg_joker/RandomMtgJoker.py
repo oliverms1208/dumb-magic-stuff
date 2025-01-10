efa0149b1d8457cde7a31799ddb12d0256e76335
Oliver Metz-Strauss <oliverms1208@gmail.com>
2025-01-10 14:14:56 -0500

    cleaned up joker a bit

--- random_mtg_joker/RandomMtgJoker.py
+++ random_mtg_joker/RandomMtgJoker.py
@@ -4,9 +4,14 @@ types = ["land", "creature", "enchantment", "artifact", "instant", "sorcery"]
 colours = ["colourless", "white", "blue", "black", "red", "green", "multicolour"]
 min_mv = 1
 max_mv = 5
+pay = 0
+usr_input = ""
 
-type = rand.choice(types)
-if type == "land":
-    print(f"The joker is a land.")
-else:
-    print(f"The joker is a {rand.choice(colours)} {type} with mana value equal to {rand.randint(min_mv, max_mv)}.")
+while usr_input.lower() not in ["quit", "q"]:
+    type = rand.choice(types)
+    if type == "land":
+        print(f"The joker is a land.")
+    else:
+        print(f"The joker is a {rand.choice(colours)} {type} with mana value equal to {rand.randint(min_mv, max_mv)}.")
+    usr_input = input(f"It costs {pay} to re-roll.\n")
+    pay += 1
